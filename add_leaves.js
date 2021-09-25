33b1164db8e2a8db160faa809bcdd93347a813a1
Tanmay Patil <tany.patil77@gmail.com>
2021-09-25 14:46:32 +0530

    added code for dynamodb table creation

--- add_leaves.js
+++ add_leaves.js
@@ -8,29 +8,60 @@ AWS.config.update({
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-console.log("Importing leaves into DynamoDB. Please wait.");
+(async function() {
+await delete_leaves();
+await import_leaves();
+})();
 
-let leaves = JSON.parse(fs.readFileSync('leaves.json', 'utf8'));
-leaves.forEach(function(leave) {
-    let params = {
-        TableName: "leaves",
-        Item: {
-            "id":  leave.id,
-            "entity_id": leave.entity_id,
-            "entity_type":  leave.entity_type
-        }
-    };
 
-    if ( leave.entity_type === "leave") {
-        params.Item.from_date = leave.from_date ;
-        params.Item.to_date = leave.to_date;
-    }
+function delete_leaves() {
+    return new Promise((resolve, reject) => {
+        var dynamodb = new AWS.DynamoDB();
+        console.log("deleting the leaves table - removing all data");
+        var params = {
+            TableName: "leaves"
+        };
+        dynamodb.deleteTable(params, function (err, data) {
+            if (err) {
+                console.error("Unable to delete table. Error JSON:", JSON.stringify(err, null, 2));
+                reject(err);
+            } else {
+                console.log("Deleted table. Table description JSON:", JSON.stringify(data, null, 2));
+                resolve();
+            }
+        });
+    })
+}
 
-    docClient.put(params, function(err, data) {
-       if (err) {
-           console.error("Unable to add leave", leave.id, ". Error JSON:", JSON.stringify(err, null, 2));
-       } else {
-           console.log("PutItem succeeded:", leave.id);
-       }
+
+function import_leaves() {
+    console.log("Importing leaves into DynamoDB. Please wait.");
+    return new Promise((resolve, reject) => {
+        let leaves = JSON.parse(fs.readFileSync('leaves.json', 'utf8'));
+        leaves.forEach(function (leave) {
+            let params = {
+                TableName: "leaves",
+                Item: {
+                    "id": leave.id,
+                    "entity_id": leave.entity_id,
+                    "entity_type": leave.entity_type
+                }
+            };
+
+            if (leave.entity_type === "leave") {
+                params.Item.from_date = leave.from_date;
+                params.Item.to_date = leave.to_date;
+            }
+
+            docClient.put(params, function (err, data) {
+                if (err) {
+                    console.error("Unable to add leave", leave.id, ". Error JSON:", JSON.stringify(err, null, 2));
+                    reject();
+                } else {
+                    console.log("PutItem succeeded:", leave.id);
+                    resolve();
+                }
+            });
+        });
     });
-});
+}
