7cdedf6dea3d6b2d9c70f81fda51d8a94ed1a216
Tanmay Patil <tany.patil77@gmail.com>
2021-10-21 23:54:13 +0530

    added slack buttons

--- send_slack.js
+++ send_slack.js
@@ -4,11 +4,47 @@ require('dotenv').config();
 function send_slack(message) {
   console.log('send_slack ' + message);
   axios.post(process.env.SLACK_ENDPOINT, {
-    text: message
-  },
-    {
-      headers: { 'Content-type': 'application/json' },
-    }
+    text: message,
+    "blocks": [
+      {
+       "type": "section",
+       "fields": [
+         {
+           "type": "mrkdwn",
+           "text":  message
+         }
+       ]
+     },
+      {
+       "type": "actions",
+       "elements": [
+                 
+         {
+           "type": "button",
+           "text": {
+             "type": "plain_text",
+             "emoji": true,
+             "text": "Paid"
+           },
+           "style": "primary",
+           "value": "click_me_123"
+         },
+         {
+           "type": "button",
+           "text": {
+             "type": "plain_text",
+             "emoji": true,
+             "text": "Snooze"
+           },
+           "style": "danger",
+           "value": "click_me_123"
+         }
+       ]
+     }
+     ]
+    ,
+      headers: { 'Content-type': 'application/json' }}
+    
   )
     .then(function (response) {
       console.log('send_slack success ' + response.statusText);
